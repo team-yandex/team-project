c42321ba6d5283f4b373e7c406eda39827ad106c
TheMerret <merret@bk.ru>
2023-04-06 19:08:20 +0300

    create project create readme rename ci/cd use dotenv create requirements

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# team-project
+[![build](https://github.com/team-yandex/team-project/actions/workflows/build.yml/badge.svg)](https://github.com/team-yandex/team-project/actions/workflows/build.yml)
+
+# What happened next?
+
+Угадайте по видео, что будет дальше.
+
+## Quick start
+
+### Clone the repository:
+```bash
+git clone https://github.com/TheMerret/intensive-django.git
+```
+
+### Create a virtual environment:
+
+Windows:
+```bash
+python -m venv venv
+```
+Mac, Linux:
+```bash
+python3 -m venv venv
+```
+
+### Activate the virtual environment:
+
+Windows:
+```bash
+cd venv/Scripts/
+```
+```bash
+.\activate
+```
+Mac, Linux:
+```bash
+source venv/bin/activate
+```
+
+### Install dependencies:
+
+Windows:
+```bash
+pip install -r requirements.txt
+```
+Mac, Linux:
+```bash
+pip3 install -r requirements.txt
+```
+
+For development:
+
+```
+requirements-dev.txt
+```
+
+For testing:
+
+```
+requirements-test.txt
+```
+
+### Configure
+
+You should use dotenv to configure settings. Example:
+
+```
+SECRET_KEY = VERYSECRETKEY
+DEBUG = false
+ALLOWED_HOSTS = 192.168.0.21,192.168.0.1
+```
+
+You can use example dotenv:
+
+```bash
+cp example.env .env
+```
+
+### Launch:
+
+#### Django server:
+
+Windows:
+```bash
+python manage.py runserver
+```
+Mac, Linux:
+```bash
+python3 manage.py runserver
+```
+## For developers
+
+[Django](https://docs.djangoproject.com/en/3.2/) documentation
---
+++ whn/whn/settings.py
@@ -0,0 +1,105 @@
+import os
+from pathlib import Path
+
+import dotenv
+
+
+dotenv.load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'notsecret')
+
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+
+if DEBUG:
+    ALLOWED_HOSTS = "*"
+else:
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost")
+ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "whn.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "whn.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "Europe/Moscow"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "/static/"
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
