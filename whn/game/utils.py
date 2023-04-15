355b2e4d15c2cc2a25e4c89d0c5242cd90d8fed0
TheMerret <merret@bk.ru>
2023-04-15 22:48:41 +0300

    move ambiguous symbols to dedicated json.

--- whn/game/utils.py
+++ whn/game/utils.py
@@ -1,61 +1,21 @@
+import json
 import re
 
-
-AMBIGUOUS = {
-    160: ' ',
-    8211: '-',
-    65374: '~',
-    65306: ':',
-    65281: '!',
-    8216: '`',
-    8217: '`',
-    8245: '`',
-    180: '`',
-    12494: '/',
-    1047: '3',
-    1073: '6',
-    1072: 'a',
-    1040: 'A',
-    1068: 'b',
-    1042: 'B',
-    1089: 'c',
-    1057: 'C',
-    1077: 'e',
-    1045: 'E',
-    1053: 'H',
-    305: 'i',
-    1050: 'K',
-    921: 'I',
-    1052: 'M',
-    1086: 'o',
-    1054: 'O',
-    1009: 'p',
-    1088: 'p',
-    1056: 'P',
-    1075: 'r',
-    1058: 'T',
-    215: 'x',
-    1093: 'x',
-    1061: 'X',
-    1091: 'y',
-    1059: 'Y',
-    65283: '#',
-    65288: '(',
-    65289: ')',
-    65292: ',',
-    65307: ';',
-    65311: '?',
-}
+import django.conf
 
 
 def normilize_string(string):
+    ambiguous_path = django.conf.settings.BASE_DIR / 'game' / 'ambiguous.json'
+    with ambiguous_path.open(mode='r') as fp:
+        ambiguous = json.load(fp)
+        ambiguous = dict((int(k), v) for k, v in ambiguous.items())
     # remove puctuaction
     normilized = re.sub(r'[^\w\s]', '', string)
     # remove spacing
     normilized = ''.join(normilized.split())
     # replace ambiguous symbols to ascii
     normilized = normilized.upper()
-    normilized = normilized.translate(AMBIGUOUS)
+    normilized = normilized.translate(ambiguous)
     normilized = normilized.lower()
-    normilized = normilized.translate(AMBIGUOUS)
+    normilized = normilized.translate(ambiguous)
     return normilized
