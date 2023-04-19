55c8fc1b49bb0b1264a254c75e9ffa2e4dc112fe
Давид <2vasyok2@gmail.com>
2023-04-19 17:08:12 +0300

    Пытаюсь исправить ошибку 1.0

--- whn/whn/settings.py
+++ whn/whn/settings.py
@@ -96,26 +96,16 @@ WSGI_APPLICATION = 'whn.wsgi.application'
 ASGI_APPLICATION = 'whn.asgi.application'
 
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': os.getenv(
-                'DB_ENGINE', default='django.db.backends.postgresql'
-            ),
-            'NAME': os.getenv('DB_NAME', default='postgres'),
-            'USER': os.getenv('POSTGRES_USER', default='postgres'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-            'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-            'PORT': os.getenv('DB_PORT', default='5432'),
-        }
-    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
