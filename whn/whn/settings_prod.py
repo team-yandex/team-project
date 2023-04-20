103563a54e05bed4205fb06515c22c0c94b1d723
TheMerret <merret@bk.ru>
2023-04-20 22:16:50 +0300

    adapt question model for s3. create production settings.

--- whn/whn/settings_prod.py
+++ whn/whn/settings_prod.py
@@ -142,6 +142,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'whathappenednext'
+AWS_S3_REGION_NAME = 'eu-central-003'
+AWS_S3_ENDPOINT_URL = f'https://s3.{AWS_S3_REGION_NAME}.backblazeb2.com'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ANSWER_BUFFER_SECONDS = 5
