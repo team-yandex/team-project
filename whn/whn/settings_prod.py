9bc513408d80e7111c91392352b51d449e2c9863
TheMerret <merret@bk.ru>
2023-04-21 20:34:10 +0300

    add prod database settings

--- whn/whn/settings_prod.py
+++ whn/whn/settings_prod.py
@@ -97,8 +97,14 @@ ASGI_APPLICATION = 'whn.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv(
+            'DB_ENGINE', default='django.db.backends.postgresql'
+        ),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
