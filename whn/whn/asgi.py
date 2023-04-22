5b9b9716a20a9be04f4b5a483fe65f1ef89c6a25
TheMerret <merret@bk.ru>
2023-04-22 23:13:38 +0300

    ignore lint in asgi. use REDIS_URL in settings

--- whn/whn/asgi.py
+++ whn/whn/asgi.py
@@ -6,13 +6,14 @@ from channels.routing import URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whn.settings')
 
 django_asgi_app = get_asgi_application()
 
 
-import game.routing
-import session.routing
+import game.routing  # noqa
+import session.routing  # noqa
 
 
 application = ProtocolTypeRouter(
